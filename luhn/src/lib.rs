331a22b9b9d246f32db2cf88426448bc8a53abad
Fo-Zi <facundozingarelli@gmail.com>
2024-11-14 19:14:49 -0300

    Solved first luhn iteration

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -2,18 +2,37 @@ use std::arch::x86_64::_CMP_FALSE_OQ;
 
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if code.len() <= 1 {
-        false
-    }
 
-    let stripped_str = code.trim();
-    if stripped_str.chars().any(|x| !x.is_alphanumeric()) {
-        false
+    let stripped_str = code.replace(char::is_whitespace, "");
+    if stripped_str.chars().any(|x| !x.is_digit(10)) {
+        return false;
     }
     
+    if stripped_str.len() <= 1 {
+        return false;
+    }
 
-}
+    let doubled_input: Vec<u32> = stripped_str
+        .chars()
+        .rev()
+        .enumerate()
+        .map(   |(index,char)| {
+            let mut num = char.to_digit(10).unwrap(); 
+            if index % 2 == 1 { 
+                num *=2;
+                if num > 9 {
+                    num -= 9;
+                }
+            }   
+            num
+        })      
+        .collect();
 
-pub fn double_every_2nd_digit(input_str: &str){
-    input_str.chars().map(|(index,x)| )
+    let sum: u64 = doubled_input.iter().map(|x| *x as u64 ).sum();
+    if sum % 10 == 0 {
+        return true;
+    }else{
+        return false;
+    }
+
 }
