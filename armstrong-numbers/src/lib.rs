6b90a304d9d946b8e90f81300ba5176f77f31f3e
Fo-Zi <facundozingarelli@gmail.com>
2024-11-14 21:19:48 -0300

    More efficient solution of Armstrong numbers

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,17 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    
-    let num_as_digits: Vec<u32> = num.
-        to_string()
+    let num_as_string = num.to_string(); 
+    let nr_digits = num_as_string.len() as u32;
+
+    num == num_as_string
         .chars()
-        .map(|x| x.to_digit(10).unwrap() as u32)
-        .collect()
-    ;
-
-    let sum_of_pwr_digits: u32 = num_as_digits
-        .iter()
-        .map(|x| x.pow(num_as_digits.len() as u32) )
-        .sum();
-
-    sum_of_pwr_digits == num 
+        .map(|x| x.to_digit(10).unwrap().pow(nr_digits) )
+        .sum()
 
 }
