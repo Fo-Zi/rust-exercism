3c12f3a19bb58e6a181549f081de7cd99f1546ef
Fo-Zi <facundozingarelli@gmail.com>
2024-11-27 19:12:06 -0300

    Adding binary-search solution

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,3 +1,16 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    let ordered_array = array.sort();
+pub fn find<T: Eq + Ord>(array: &[T], key: T) -> Option<usize> {
+    let mut index_low = 0;
+    let mut index_high = array.len();
+
+    while index_low < index_high {
+        let mid = index_low + (index_high - index_low) / 2;
+
+        match &array[mid] {
+            elem if *elem == key => return Some(mid),
+            elem if *elem < key => index_low = mid + 1,
+            _ => index_high = mid,
+        }
+    }
+
+    None
 }
